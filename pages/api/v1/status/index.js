86e04adedb0efadb86e669ff1b2c515adac82c58
Fabrício Rocha <90473184+fabricior0cha@users.noreply.github.com>
2025-12-17 02:19:20 +0000

    adiciona `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const res = await database.query("SELECT 1+1 AS result");
+  console.log(res.rows);
   response.status(200).json({ status: "ok" });
 }
 export default status;
