6c14b9c8ab50f0ff2634fd912848c441ac914e9f
Fabrício Rocha <90473184+fabricior0cha@users.noreply.github.com>
2026-01-21 10:56:31 +0000

    finaliza o endpoint `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,31 @@
 import database from "../../../../infra/database.js";
 
 async function status(request, response) {
-  const res = await database.query("SELECT 1+1 AS result");
-  console.log(res.rows);
-  response.status(200).json({ status: "ok" });
+  const updatedAt = new Date().toISOString();
+  const dbName = process.env.POSTGRES_DB;
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersion = dbVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+
+  const openedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+
+  const openedConnections = openedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,17 @@
 test("GET /api/v1/status should return status code 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
+  const body = await response.json();
+  console.log(body);
   expect(response.status).toBe(200);
+  expect(body.updated_at).toBeDefined();
+
+  const parsedDate = new Date(body.updated_at).toISOString();
+  expect(parsedDate).toBe(body.updated_at);
+
+  expect(body.dependencies.database.version).toBeDefined();
+  expect(body.dependencies.database.version).toBe("16.0");
+
+  expect(body.dependencies.database.max_connections).toBe(100);
+  expect(body.dependencies.database.opened_connections).toBe(1);
 });
